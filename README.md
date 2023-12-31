3698887d1de455a39690d4047062e80019677680
BrenoLira01 <brenoczbreno@gmail.com>
2023-12-31 15:39:11 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Introdução
+Este é um projeto simples desenvolvido para estudos de switch case em JavaScript. A proposta é interativa: ao clicar em um botão, a interface do usuário reage, demonstrando a funcionalidade e a aplicação do comando switch case.
+
+## Visualizar
+
+Para ver o projeto em ação, acesse o link [aqui](), onde você poderá interagir com a demonstração.
+
+## Demonstração do Projeto
+
+![Imagem de Exemplo]()
+
+## Autor
+Nome: Francisco Breno
+
+## Contato
+[![Email](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](mailto:franciscobrenolira@gmail.com)
---
+++ script/script.js
@@ -0,0 +1,35 @@
+let frutas = document.querySelectorAll(".fruta");
+
+frutas.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+
+        let corBotao = document.querySelector(".cor");
+
+        switch (botao.innerText) {
+            case "🍌":
+                corBotao.style.background = "#FBCD63";
+                break;
+
+            case "🍉":
+                corBotao.style.background = "#F36049";
+                break;
+
+            case "🍇":
+                corBotao.style.background = "#835AB8";
+                break;
+
+            case "🍊":
+                corBotao.style.background = "#F8762A";
+                break;
+
+            case "🍓":
+                corBotao.style.background = "#DC320F";
+                break;
+
+            case "🍐":
+                corBotao.style.background = "#CBE042";
+                break;
+        }
+
+    });
+});
